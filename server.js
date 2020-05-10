484407cdb507f3e85d8955343f797b36ad04b9a1
unknown <Jamal Roberts>
2020-05-10 13:48:34 -0400

    Add procfile

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const app = require('./app');
 
 const port = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  
+    const path = require('path');
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+  
+  }
+
 const server = http.createServer(app);
 
 server.listen(port);
